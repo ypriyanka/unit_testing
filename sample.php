913d517d542d581efbcb466501c9d7a469366117
y.priyanka <ypriyankay@gmail.com>
2014-04-16 18:40:55 +0530

    exception commit

--- sample.php
+++ sample.php
@@ -1,8 +1,10 @@
 <?php
 class User {
-    protected $name;
+   public $name;
+   public $stack =array();
  
     public function getName() {
+   
         return $this->name;
     }
  
@@ -11,6 +13,39 @@ class User {
     }
  
     public function talk() {
+    	
         return "Hello world!";
     }
+	public function checkArray()
+	{
+		
+		return $this->stack;
+	}
 }
+class User2 extends User
+{
+	 
+	public function talk2()
+	{
+      
+	 return $this->name;
+	}
+	
+}
+class Foo {
+
+	public    $foo  = 1;
+	public      $go=4;
+    protected $bar  = 2;
+    private   $baz  = 3;
+}
+class MyException
+
+{
+public function testException()
+{
+	
+}	
+}
+
+?>
--- testsample.php
+++ testsample.php
@@ -1,12 +1,13 @@
 <?php
-require_once "home/bigc/composer/vendor/autoload.php";
+//require_once "home/bigc/composer/vendor/autoload.php";
 require_once "sample.php";
  
 class UserTest extends PHPUnit_Framework_TestCase
 {
+	
 	// test the talk method
-    public function testTalk()
-	 {
+    public function test1()
+	{
         // make an instance of the user
         $user = new User();
 		
@@ -14,7 +15,7 @@ class UserTest extends PHPUnit_Framework_TestCase
 		 {
         $this->user = new User();
         $this->user->setName("Tom");
-        }
+        } 
 				
 		protected function tearDown()
 		 {
@@ -25,6 +26,82 @@ class UserTest extends PHPUnit_Framework_TestCase
         $expected = "Hello world!";
         $actual = $user->talk();
         $this->assertEquals($expected, $actual);
-	  }
+	}
+	public function testVariables()
+	{
+		$user1 = new User();
+		$user1-> setName ('Gerald');
+    	$this -> assertTrue ($user1->getName() == 'Gerald');
+	}
+	
+	/*public function testCount()
+	{
+		$this->assertCount(0, array(new Foo(),new UserTest()));
+	}*/
+	/*public function testInsatances()
+	{
+		$test=array();
+	$too = new Foo();
+		$this->assertContainsOnlyInstancesOf('Foo', array(new Foo(), $test, $too));
+	} */
+	
+	
+public function testPublic1()
+{
+	 $test=array();
+	$too = new Foo();
+	$reflect = new ReflectionClass($too);
+	$props   = $reflect->getProperties(ReflectionProperty::IS_PUBLIC);
+	foreach ($props as $prop) 
+	{
+	    $test[]=$prop->getName();
+	}
+	$this->assertContains('foo',$test);
 }
+public function testPrivate()
+{
+	 $test=array();
+	$too = new Foo();
+	$reflect = new ReflectionClass('Foo');
+	$props   = $reflect->getProperties(ReflectionProperty::IS_PRIVATE);
+	foreach ($props as $prop) {
+	    $test[]=$prop->getName();
+		}
+		$this->assertContains('baz',$test);
+	}
+
+public function testgetMethod() {
+  $class = new ReflectionClass('User2');
+  $method = $class->getMethod('talk');
+  $method->setAccessible(true);
+  return $method;
+	}
+
+public function teststringmatches()
+{
+	$this->assertRegExp('/foo/', 'foo bar');
+
+	if(1==1)
+	{
+		try{
+			$this->assertContains('foo','bar');
+		}
+		catch (Exception $e)
+		{
+			throw new Exception("first exception", 1);
+			
+		}
+	}
+}
+    /**
+     * @expectedException        InvalidArgumentException
+     * @expectedExceptionMessage second exception
+     */
+    public function testExceptionHasRightMessage()
+    {
+        throw new InvalidArgumentException("second exception", 10);
+    }
+}
+
+
 ?>
